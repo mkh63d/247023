6e75fa3be6d816562771914d838d01bec3666c03
mkh63d <mkh63d@gmail.com>
2023-04-05 17:28:37 +0200

    1

--- Task2.cpp
+++ Task2.cpp
@@ -13,29 +13,33 @@ int main()
 	pokeb(0xB800, 0, 'A'); //make an A sign on screen 
 	pokeb(0xB800, 1, 0x0F); // with a 0x0F foreground 
 
-	do
+	While(1)
 	{
-		c = inportb(0x60); //take an input from keyboard when it is hit
-		switch (c)
+		do
 		{
-		case 0x1B: //escape = quit program
-			break;
-		case 0x48:
-			if (y > -1) gotoxy(x, y - 1);	//goes up
-		case 0x4B:
-			if (x > 0) gotoxy(x - 1, y);	//goes left 
-		case 0x4D:
-			if (x < 81) gotoxy(x + 1, y);	//goes right
-		case 0x50:
-			if (y < 26) gotoxy(x, y + 1);	//goes down
-		case '+':
-			//change foreground
-		case '-':
-			textbackground(0x01);
-		}
-	} while (_kbhit());
-	//getch();
-
+			c = inportb(0x60); //take an input from keyboard when it is hit
+			switch (c)
+			{
+			case 0x1B: //escape = quit program
+				break;
+			case 0x48:
+				if (y > -1) gotoxy(x, y - 1);	//goes up
+			case 0x4B:
+				if (x > 0) gotoxy(x - 1, y);	//goes left 
+			case 0x4D:
+				if (x < 81) gotoxy(x + 1, y);	//goes right
+			case 0x50:
+				if (y < 26) gotoxy(x, y + 1);	//goes down
+			case '+':
+				//change foreground
+			case '-':
+				textbackground(0x01);
+			}
+		} while (_kbhit());
+		char p = getch();
+		pokeb(0xB801, 0, p);
+		pokeb(0xB801, 1, 0xF0);
+	}
 	_setcursortype(_NORMALCURSOR); //set back to normal
 	return 0;
 }
